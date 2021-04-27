a2be9a2802b80e3ca9f6582ed3fd498fb09cbc08
cubecloud <rainmaverick@mail.ru>
2021-04-27 10:07:43 +0300

    # cardgame.py with AI 0.0054

---
+++ README.md
@@ -0,0 +1,2 @@
+The "fool" - card game with learning AI.
+This source created for testing purpose.  
--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
-setup(
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='cardgame',
-    version='0.0.0.53',
-    packages=['cardgame'],
-    url='',
-    license='',
-    author='Oleg Novokshonov',
+    version='0.0.54',
+    # name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    author='cubecloud',
     author_email='rainmaverick@mail.ru',
+    description='The "fool" - card game with learned AI',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cubecloud/fool_game",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": ""},
+    python_requires=">=3.7",
     install_requires=['setuptools>=51.0.0',
                       'tensorflow==2.3.0',
                       'numpy==1.18.5',
@@ -17,4 +29,9 @@ setup(
                       'pandas>=1.1.5',
                       'ipython>=5.5.0',
                       ]
+)
+
+
+setup(
+
     )
