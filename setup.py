e613f7ae152367a6f4259cee0ca14f052ad12869
cubecloud <rainmaverick@mail.ru>
2021-05-10 14:24:20 +0300

    # foolgame.py == 0.1.97 ! finaly fixed error with Deck.what_suit method - "TypeError: unhashable type: 'list'"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='fool_game',
-    version='0.1.96',
+    version='0.1.97',
     packages=['cardgames'],
     # name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
     author='cubecloud',
